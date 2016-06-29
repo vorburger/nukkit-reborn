50b9cade7dfc2826bd36a0e97599381dcf4112a1
Snake1999 <admin@snake1999.com>
2016-06-29 21:07:23 +0800

    lol

---
+++ nukkit-std-minecraft/src/main/java/org/nukkitmc/nukkitstd/minecraft/io/ClientChunk.java
@@ -0,0 +1,10 @@
+package org.nukkitmc.nukkitstd.minecraft.io;
+
+import org.nukkitmc.nukkit.core.io.ClientSendable;
+
+/**
+ * Created by Mulan Lin('Snake1999') on 2016/6/29 21:06.
+ */
+public interface ClientChunk extends ClientSendable {
+
+}
